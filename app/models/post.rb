27a23ae14b575306d4b2f60553a9efe621779358
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-28 21:32:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length:{minimum: 400}
+  validates :content, length:{ minimum: 400 }
   validates :category, inclusion:{ in: %w(Fiction Non-Fiction)}
 end
